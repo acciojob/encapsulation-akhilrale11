0f7076e7231004619f7e9ed231fdda9fc00e08df
akhilrale11 <117888290+akhilrale11@users.noreply.github.com>
2024-11-09 09:41:13 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3: Create an object of class RWOnly
+        RWOnly obj = new RWOnly();
+        
+        // Task 4: Try setting and getting the value of `name` directly
+        // Uncommenting the following lines will cause errors
+        // obj.name = "John"; // Error: name has private access in RWOnly
+        // System.out.println(obj.name); // Error: name has private access in RWOnly
+
+        // Task 6: Use the setter function to set a value and getter function to access it
+        obj.setName("John");
+        System.out.println("Name: " + obj.getName());
+    }
 }
